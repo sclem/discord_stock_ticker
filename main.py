d853786d45d2582339db5ad37f2b802f67820aa3
sclem <clem.spencer@gmail.com>
2021-04-23 17:59:07 +0000

    add report commands

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import os
 import re
+from datetime import datetime
+from pytz import timezone, utc
 
 import discord
+from discord.ext import commands
 from yahoo import get_stock_price
 
-client = discord.Client()
-
+COMMAND_PREFIX='!'
 TARGET_CHANNEL = os.getenv('DISCORD_CHANNEL_ID')
-
+TARGET_ROLE = os.getenv('DISCORD_ROLE_ID')
 STOCK_REGEX = "\\b([A-Z]{3,5})\\b"
 prog = re.compile(STOCK_REGEX)
-
-TARGET_ROLE = os.getenv('DISCORD_ROLE_ID')
+bot = commands.Bot(command_prefix=COMMAND_PREFIX)
+eastern_tz = timezone('America/New_York')
 
 print('checking for role {}'.format(TARGET_ROLE))
 
@@ -27,24 +29,27 @@ def get_price(ticker):
         return None
 
 
-@client.event
+@bot.event
 async def on_ready():
-    print('we have logged in as {0.user}'.format(client))
+    print('we have logged in as {0.user}'.format(bot))
 
-
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
     if str(message.channel.id) != TARGET_CHANNEL:
         return
-    has_role = False
-    for role in message.author.roles:
-        if str(role.id) == TARGET_ROLE:
-            has_role = True
-            break
-    if not has_role:
-        print('{} does not have role'.format(message.author.name))
+    if len(TARGET_ROLE) > 0:
+        has_role = False
+        for role in message.author.roles:
+            if str(role.id) == TARGET_ROLE:
+                has_role = True
+                break
+        if not has_role:
+            print('{} does not have role'.format(message.author.name))
+            return
+    if message.content.startswith(COMMAND_PREFIX):
+        await bot.process_commands(message)
         return
 
     tickers = prog.findall(message.content)
@@ -68,6 +73,84 @@ async def on_message(message):
     print('sending: \n{}'.format(out_msg))
     await message.channel.send(out_msg)
 
+def get_market_times_utc():
+    now = datetime.now()
+    open_time = datetime(now.year, now.month, now.day, 9, 30)
+    open_time = eastern_tz.localize(open_time, is_dst=None).astimezone(utc).replace(tzinfo=None)
+    close_time = datetime(now.year, now.month, now.day, 16, 0)
+    close_time = eastern_tz.localize(close_time, is_dst=None).astimezone(utc).replace(tzinfo=None)
+    return [open_time, close_time]
+
+
+@bot.command(name='report')
+async def report(ctx, command):
+    check_ticker = None
+    if len(ctx.message.mentions) == 0:
+        check_ticker = command 
+        if len(check_ticker) == 0:
+            print('no ticker')
+            return
+    # scan messages for ticker ref
+    async with ctx.message.channel.typing():
+        [open_time, close_time] = get_market_times_utc()
+        out_msg = ''
+        if check_ticker:
+            ticker_data = get_price(check_ticker) 
+            if not ticker_data:
+                await ctx.send('cannot find {}'.format(check_ticker))
+                return
+            # valid ticker, get mentions
+            ticker_map = dict()
+            count = 0
+            async for msg in ctx.message.channel.history(before=close_time, after=open_time):
+                if msg.author == bot.user:
+                    continue
+                if msg.content.startswith(COMMAND_PREFIX):
+                    continue
+
+                tickers = prog.findall(msg.content)
+                for t in tickers:
+                    if not t in ticker_map:
+                        ticker_map[t] = 0
+                    ticker_map[t] += 1
+
+            if check_ticker in ticker_map:
+                count = ticker_map[check_ticker]
+            out_msg += 'During market hours today, {} has been mentioned {} times'.format(check_ticker, count)
+        else:
+            # user report
+            target_user = ctx.message.mentions[0]
+            ticker_map = dict()
+            async for msg in ctx.message.channel.history(before=close_time, after=open_time):
+                if msg.author != target_user:
+                    continue
+                if msg.content.startswith(COMMAND_PREFIX):
+                    continue
+                tickers = prog.findall(msg.content)
+                for t in tickers:
+                    if not t in ticker_map:
+                        ticker_map[t] = 0
+                    ticker_map[t] += 1
+
+            ticker_map_sorted = dict(sorted(ticker_map.items(), key=lambda item: item[1], reverse=True))
+            out_msg += 'During market hours today, <@{}> has mentioned:\n'.format(target_user.id)
+            count = 0
+
+            for k, v in ticker_map_sorted.items():
+                ticker_data = get_price(k)
+                # make sure valid
+                if ticker_data:
+                    count += 1
+                    plural = ''
+                    if v > 1:
+                        plural = 's'
+                    out_msg += '{} {} time{}\n'.format(k, v, plural)
+
+            if count == 0:
+                out_msg = '<@{}> is a gay bear'.format(target_user.id)
+
+        print('sending: \n{}'.format(out_msg))
+        await ctx.send(out_msg)
 
 TOKEN = os.getenv('DISCORD_TOKEN')
-client.run(TOKEN)
+bot.run(TOKEN)
